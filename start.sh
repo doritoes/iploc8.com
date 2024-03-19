8d092793e781792760a3a0b8a5bd515e715c1b10
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-19 16:12:28 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Start MySQL in the background
-/usr/local/bin/docker-entrypoint.sh mysqld &
+/usr/local/bin/docker-entrypoint.sh mysqld --local-infile=1 &
 # Wait for MySQL to initialize
 until mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" &> /dev/null; do
   echo >&2 "MySQL is unavailable - sleeping"
   sleep 2
 done
 echo >&2 "MySQL is up - starting data import"
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "
+mysql --local-infile=1 --skip-password-warning -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "
     USE mydatabase;
     LOAD DATA LOCAL INFILE '/app/countries.csv'
         INTO TABLE countries
