f067548626c4e3c8c1ce42dd0dfed2cbf88b97d1
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-19 12:06:35 -0400

    Update start-test.sh

--- start-test.sh
+++ start-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start MySQL
 echo >&2 "About to start mysqld"
-/usr/local/bin/docker-entrypoint.sh mysqld
+/usr/local/bin/docker-entrypoint.sh mysqld &
 echo >&2 "Finished starting mysqld"
 # Wait for MySQL to initialize
 until mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" &> /dev/null; do
