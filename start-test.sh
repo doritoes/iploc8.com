77b919d614a7c1f60410a6f0687406870b44ebe7
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-19 12:04:16 -0400

    Update start-test.sh

--- start-test.sh
+++ start-test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Start MySQL
+echo >&2 "About to start mysqld"
 /usr/local/bin/docker-entrypoint.sh mysqld
+echo >&2 "Finished starting mysqld"
 # Wait for MySQL to initialize
 until mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" &> /dev/null; do
   echo >&2 "MySQL is unavailable - sleeping"
   sleep 2
 done
-
 echo >&2 "MySQL is up - executing command"
-nohup /usr/bin/python3 -m app flask --host=0.0.0.0 &
+python3 app.py &
