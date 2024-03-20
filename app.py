32adf1cf30392d3153dc12393cb918e7210f3393
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-19 21:51:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -66,15 +66,12 @@ def get_ip():
             SELECT g.country, c.Name AS country_long
             FROM geo g
             LEFT JOIN countries c ON g.country = c.Code
-            LEFT JOIN sanctions s ON c.Code = s.Country 
             WHERE g.start <= %s AND g.end >= %s
-            GROUP BY g.country
         """, (ip_decimal, ip_decimal))
         result = cursor.fetchone()
         if result:
             country = result[0]
             country_long = result[1] if result[1] else "Unknown"  # Handle missing country_long
-            sanction = result[2] if result[2] else ""
         else:
             country = "Unknown"
             country_long = "Unknown"
@@ -94,8 +91,8 @@ def get_ip():
         'country_long': country_long,
         'isp': isp
     }
-    if sanction:
-        output_data['sanction'] = sanction
+    # if sanction:
+        # output_data['sanction'] = sanction
     return jsonify(output_data)
 
 if __name__ == '__main__':
