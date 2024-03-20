81d8c3b8dc7ac44fdf95713d7d4a52e5624d1b54
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-20 17:41:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -181,15 +181,6 @@ def ip_info():
             country = "Unidentified"
             country_long = "Unidentified"
             state1 = state2 = city = postcocde = latitude = longitude = timezone = ""
-        """
-        cursor.execute("""
-            SELECT description 
-            FROM asn 
-            WHERE start <= %s AND end >= %s
-        """, (ip_decimal, ip_decimal))
-        asn_result = cursor.fetchone()
-        isp = asn_result[0] if asn_result else "Unknown"
-        """
         isp = "disabled for testing"
     except Exception as e:
         print(f"Error encountered: {e}")
