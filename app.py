cd50086889a9c33d8490b168a6c2dcd29855bc26
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-20 18:20:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -172,6 +172,13 @@ def ip_info():
         if result:
             country = result[0] if result[0] else "Unknown"
             country_long = result[1] if result[1] else "Unknown"
+            state1 = result[2] if result[2] else ""
+            state2 = result[3] if result[3] else ""
+            city = result[4] if result[4] else ""
+            postcode = result[5] if result[5] else ""
+            latitude = result[6] if result[6] else ""
+            longitude = result [7] if result[7] else ""
+            timezone = result[8] if result[8] else ""
         else:
             country = "Unidentified"
             country_long = "Unidentified"
