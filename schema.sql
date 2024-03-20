7a80684b4d2c209fe59abb60e99ac46d516294b5
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-20 10:53:31 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -35,3 +35,19 @@ CREATE TABLE IF NOT EXISTS sanctions (
     Country CHAR(2) PRIMARY KEY,
     Sanction VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE city (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    start BIGINT NOT NULL,
+    end BIGINT NOT NULL,
+    country_code CHAR(2),
+    state1 VARCHAR(255),
+    state2 VARCHAR(255), 
+    city VARCHAR(255),
+    postcode VARCHAR(128),
+    latitude VARCHAR(10),
+    longitude VARCHAR(10), 
+    timezone VARCHAR(32),
+    INDEX (start),
+    INDEX (end)
+);
