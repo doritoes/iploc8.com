85527dd36a65af82fd27f9da83e2f26c4dded3e0
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-25 13:46:10 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,6 +13,8 @@ curl -s -o iptoasn-asn-ipv4-num.csv  https://cdn.jsdelivr.net/npm/@ip-location-d
 curl -s -o geo-whois-asn-country-ipv4-num.csv  https://cdn.jsdelivr.net/npm/@ip-location-db/geo-whois-asn-country/geo-whois-asn-country-ipv4-num.csv
 curl -s -o asn-ipv4-num.csv https://cdn.jsdelivr.net/npm/@ip-location-db/asn/asn-ipv4-num.csv
 curl -s -L -o dbip-city-ipv4-num.csv.gz https://unpkg.com/@ip-location-db/dbip-city/dbip-city-ipv4-num.csv.gz
+curl -s -o broadcom.json https://servicepoints.threatpulse.com/
+curl -s -o zscaler.json https://config.zscaler.com/api/zscaler.net/cenr/json
 # Import local files
 echo >&2 "importing countries.csv"
 mysql --local-infile=1 -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "
@@ -70,6 +72,15 @@ gunzip /app/dbip-city-ipv4-num.csv.gz && mysql --local-infile=1 -uroot -p"${MYSQ
     LINES TERMINATED BY '\n'
     (start, end, country_code, state1, state2, city, postcode, latitude, longitude, timezone);
 " && rm /app/dbip-city-ipv4-num.csv
+echo >&2 "importing zscaler.json"
+zscaler.py && && mysql --local-infile=1 -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "
+    USE mydatabase;
+    LOAD DATA LOCAL INFILE '/app/zscaler.csv'
+    INTO TABLE corporate
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '\"'
+    LINES TERMINATED BY '\n'
+    (type, vendor, start, end, city, node);
 # Start the app
 echo >&2 "download complete - starting app"
 python3 app.py
